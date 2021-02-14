a31065061a9acac0198bbaf5dedc59da9ffc4c13
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-02-14 23:17:01 +1100

    Added code for tech specs page

--- project-base/main.js
+++ project-base/main.js
@@ -1,6 +1,28 @@
-(() => {
+// CODE TO OPEN MODAL DIALOG BOX
+// (() => {
     const dialog = document.querySelector('.dialog-overview');
     const openButton = document.querySelector('#buy-btn');
 
     openButton.addEventListener('click', () => dialog.show());
-  })();
+//   })();
+
+// CODE TO OPEN TECH SPEC "DRAWERS"
+const specHeadings = document.querySelectorAll('section')
+
+
+// JS WHERE ADDS STYLE PARAMETER --- IN PROPOSAL PLUS SPINS BEFORE BECOMING MINUS
+specHeadings.forEach(heading => {
+    heading.addEventListener('click', ()=>{
+        if(heading.querySelector('ul').style.display === 'block'){
+            heading.querySelector('ul').style.display = 'none'
+            heading.querySelector('i').classList.remove('fa-minus')
+            heading.querySelector('i').classList.add('fa-plus')
+        }else{
+            heading.querySelector('ul').style.display = 'block'
+            heading.querySelector('i').classList.remove('fa-plus')
+            heading.querySelector('i').classList.add('fa-minus')
+        }
+        
+    })
+});
+
