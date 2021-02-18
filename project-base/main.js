d95b5e5741bd6cfe15af82f7adc56319a15f21db
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-02-18 18:01:17 +1100

    Added carousel, progress bar, changed specs toggle

--- project-base/main.js
+++ project-base/main.js
@@ -12,17 +12,57 @@ const specHeadings = document.querySelectorAll('section')
 
 // JS WHERE ADDS STYLE PARAMETER --- IN PROPOSAL PLUS SPINS BEFORE BECOMING MINUS
 specHeadings.forEach(heading => {
+    // heading.addEventListener('click', ()=>{
+    //     if(heading.querySelector('ul').style.display === 'block'){
+    //         heading.querySelector('ul').style.display = 'none'
+    //         heading.querySelector('i').classList.remove('fa-minus')
+    //         heading.querySelector('i').classList.add('fa-plus')
+    //     }else{
+    //         heading.querySelector('ul').style.display = 'block'
+    //         heading.querySelector('i').classList.remove('fa-plus')
+    //         heading.querySelector('i').classList.add('fa-minus')
+    //     }
+        
+    // })
+
     heading.addEventListener('click', ()=>{
-        if(heading.querySelector('ul').style.display === 'block'){
-            heading.querySelector('ul').style.display = 'none'
-            heading.querySelector('i').classList.remove('fa-minus')
-            heading.querySelector('i').classList.add('fa-plus')
-        }else{
-            heading.querySelector('ul').style.display = 'block'
-            heading.querySelector('i').classList.remove('fa-plus')
-            heading.querySelector('i').classList.add('fa-minus')
-        }
+            heading.querySelector('ul').classList.toggle('open')
+            heading.querySelector('i').classList.toggle('fa-minus')
+            heading.querySelector('i').classList.toggle('fa-plus')
         
     })
 });
 
+// JS FOR PROGRESS BAR FEATURES PAGE
+const progressBar = document.querySelector('#bar');
+
+window.addEventListener('scroll', () => {
+
+    if (progressBar !== null){ // Removes error message on other pages
+        console.log(window.pageYOffset/(document.body.clientHeight-innerHeight));
+        progressBar.percentage = window.pageYOffset/(document.body.clientHeight-innerHeight)*100;
+    }
+
+
+});
+
+// INTERFACE PAGE
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
