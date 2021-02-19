d10ea634e6772d65dca59130b6a931a96a3f2ee7
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-02-19 23:40:57 +1100

    Removed code not applicable to all pages

---
+++ project-base/scripts/main.js
@@ -0,0 +1,9 @@
+//COMMON TO ALL PAGES
+
+// CODE TO OPEN MODAL DIALOG BOX
+// (() => {
+    const dialog = document.querySelector('.dialog-overview');
+    const openButton = document.querySelector('#buy-btn');
+
+    openButton.addEventListener('click', () => dialog.show());
+//   })();
