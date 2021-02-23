ed9dfb0782c3d1754b56dfc14d9c1def4f6e7f0c
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-02-23 22:45:59 +1100

    Added mobile menu code

--- project-base/scripts/main.js
+++ project-base/scripts/main.js
@@ -7,3 +7,15 @@
 
     openButton.addEventListener('click', () => dialog.show());
 //   })();
+
+
+//CODE TO OPEN MOBILE MENU
+const hamburgerBtn = document.querySelector('.hamburger-button')
+const nav = document.querySelector('nav')
+const openClose = hamburgerBtn.querySelector('.fa-bars')
+
+hamburgerBtn.addEventListener('click', function(){
+    nav.classList.toggle('open')
+    openClose.classList.toggle('fa-bars')
+    openClose.classList.toggle('fa-times')
+})
