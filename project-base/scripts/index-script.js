ddca13709aaa09fc583cc4a037f7db79732cb8b4
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-02-24 21:22:22 +1100

    Added comments and simplified redundant code

--- project-base/scripts/index-script.js
+++ project-base/scripts/index-script.js
@@ -3,67 +3,45 @@
   //GSAP INDEX MAIN ANIMATION
   let tlMain = gsap.timeline({delay: 1})
 
-  tlMain.from('#img1', {
-    duration: 1,
-    x: 2000,
-    y: -700
-  })
-  .from('#perform-heading',{
-    duration: 1,
-    opacity: 0
-  }, "-=0.2")
-  .from('#img2', {
-    duration: 1,
-    x: 2000,
-    y: -700
-  })
-  .from('#browse-heading',{
-    duration: 1,
-    opacity: 0
-  }, "-=0.2")
-  .from('#img3', {
-    duration: 1,
-    x: 2000,
-    y: -700
-  })
-  .from('#edit-heading',{
-    duration: 1,
-    opacity: 0
-  }, "-=0.2")
-  .from('#img4', {
-    duration: 1,
-    x: 2000,
-    y: -700
-  })
-  .from('#sample-heading',{
-    duration: 1,
-    opacity: 0
-  }, "-=0.2")
-  .from('#img5', {
-    duration: 1,
-    x: 2000,
-    y: -700
-  })
-  .from('#build-heading',{
-    duration: 1,
-    opacity: 0
-  }, "-=0.2")
-  .from('#img6', {
-    duration: 1,
-    x: 2000,
-    y: -700
-  })
-  .from('#mix-heading',{
-    duration: 1,
-    opacity: 0
-  }, "-=0.2")
-  .from('.main-heading',{
+  // Function to slide each interface image into view
+  function imageSlide(imageNumber){
+    tlMain.from(imageNumber, {
+      duration: 1,
+      x: 2000,
+      y: -700
+    })
+  }
+
+  // Function to fade each subheading in
+  function headingFadeIn(headingName){
+    tlMain.from(headingName,{
+      duration: 1,
+      opacity: 0
+    }, "-=0.2")
+  }
+
+  // Calls each function with respective image/subheading
+  imageSlide('#img1')
+  headingFadeIn('#perform-heading')
+  imageSlide('#img2')
+  headingFadeIn('#browse-heading')
+  imageSlide('#img3')
+  headingFadeIn('#edit-heading')
+  imageSlide('#img4')
+  headingFadeIn('#sample-heading')
+  imageSlide('#img5')
+  headingFadeIn('#build-heading')
+  imageSlide('#img6')
+  headingFadeIn('#mix-heading')
+
+  // Animate main heading and seperators
+  tlMain.from('.main-heading',{
     duration: 1.2,
     // opacity:0,
     scale: 0,
     ease: "elastic.out(1, 0.6)"
   })
-  .from('.separator',{
+  tlMain.from('.separator',{
     duration: 2,
     opacity: 0
   })
