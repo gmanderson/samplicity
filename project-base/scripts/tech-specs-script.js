b4a8c93011a2c32cb4d110f158221f6fd769e9a6
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-02-24 15:37:52 +1100

    Removed commented out code

--- project-base/scripts/tech-specs-script.js
+++ project-base/scripts/tech-specs-script.js
@@ -1,22 +1,9 @@
-// CODE TO OPEN TECH SPEC "DRAWERS"
-const specHeadings = document.querySelectorAll('section')
+//THIS FILE CONTAINS ONLY SCRIPTS SPECIFIC TO INTERFACE PAGE
 
+const specHeadings = document.querySelectorAll('section')
 
 // JS WHERE ADDS STYLE PARAMETER --- IN PROPOSAL PLUS SPINS BEFORE BECOMING MINUS
 specHeadings.forEach(heading => {
-    // heading.addEventListener('click', ()=>{
-    //     if(heading.querySelector('ul').style.display === 'block'){
-    //         heading.querySelector('ul').style.display = 'none'
-    //         heading.querySelector('i').classList.remove('fa-minus')
-    //         heading.querySelector('i').classList.add('fa-plus')
-    //     }else{
-    //         heading.querySelector('ul').style.display = 'block'
-    //         heading.querySelector('i').classList.remove('fa-plus')
-    //         heading.querySelector('i').classList.add('fa-minus')
-    //     }
-        
-    // })
-
     heading.addEventListener('click', ()=>{
             heading.querySelector('ul').classList.toggle('open')
             heading.querySelector('.fa-plus').classList.toggle('spin-animation')
