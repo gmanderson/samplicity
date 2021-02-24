c8e0cffba0de087f37ca81cc78d0732da7ea856d
s3318814 <61717348+gerardmanderson@users.noreply.github.com>
2021-02-24 16:54:50 +1100

    Reduced indentation

--- project-base/scripts/tech-specs-script.js
+++ project-base/scripts/tech-specs-script.js
@@ -5,8 +5,8 @@ const specHeadings = document.querySelectorAll('section')
 // JS WHERE ADDS STYLE PARAMETER --- IN PROPOSAL PLUS SPINS BEFORE BECOMING MINUS
 specHeadings.forEach(heading => {
     heading.addEventListener('click', ()=>{
-            heading.querySelector('ul').classList.toggle('open')
-            heading.querySelector('.fa-plus').classList.toggle('spin-animation')
-            heading.querySelector('.fa-minus').classList.toggle('fade-in')
+        heading.querySelector('ul').classList.toggle('open')
+        heading.querySelector('.fa-plus').classList.toggle('spin-animation')
+        heading.querySelector('.fa-minus').classList.toggle('fade-in')
     })
 });
